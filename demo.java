67603589096e878c72b43ebcb4e918a1eb9fffd9
VincentH-APCS <vhuang10@stuy.edu>
2020-12-05 20:54:32 -0500

    Testing for an exception when the end has been reached

--- demo.java
+++ demo.java
@@ -9,7 +9,7 @@ public static void main(String[] args){
     }
   }
   System.out.println();
-  //System.out.println(r.next());
+  System.out.println(r.next());
 //  System.out.println(r.next());
 }
 
