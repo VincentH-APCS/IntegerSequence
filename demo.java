22159af4184313b23be7ac56deab341dedc69c45
VincentH-APCS <vhuang10@stuy.edu>
2020-12-05 20:50:06 -0500

    Took out the lns in the prints in the while loop, but it works! now for additional tests

--- demo.java
+++ demo.java
@@ -3,12 +3,14 @@ public class demo{
 public static void main(String[] args){
   IntegerSequence r = new Range(10, 15);
   while(r.hasNext()){
-    System.out.println(r.next());
+    System.out.print(r.next());
     if(r.hasNext() ){
-      System.out.println( " , ");
+      System.out.print( " , ");
     }
   }
   System.out.println();
+  //System.out.println(r.next());
+//  System.out.println(r.next());
 }
 
 }
