08e91e81aa79c405e7d807671f8a7136d3086152
VincentH-APCS <vhuang10@stuy.edu>
2020-12-05 20:36:55 -0500

    Fixed next to try and catch hasNext for the NoSuchElementException, and added a main to test, will test now

--- IntegerSequence.java
+++ IntegerSequence.java
@@ -26,10 +26,22 @@ public class Range implements IntegerSequence{
   }
 
   public int next(){
-    if(current >= end){
-      throws NoSuchElementException;
+    try{ this.hasNext();
+    } catch (NoSuchElementException){
+      System.out.println("There is no Next Element");
     }
     current = current++;
     return current - 1;
   }
+}
+
+public static void main(String[] args){
+  IntegerSequence r = new Range(10, 15);
+  while(r.hasNext()){
+    System.out.println(r.next())
+    if(r.hasNext() ){
+      System.out.println( " , ");
+    }
+  }
+  System.out.println();
 }
