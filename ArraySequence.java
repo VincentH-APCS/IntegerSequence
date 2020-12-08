c296fbd2d180014903cf7db9bd529fa8aea3781e
VincentH-APCS <vhuang10@stuy.edu>
2020-12-08 12:48:12 -0500

    Wrote in the first constructor and the 4 methods from the interface to be tested

--- ArraySequence.java
+++ ArraySequence.java
@@ -3,5 +3,28 @@ public class ArraySequence implements IntegerSequence{
   private int currentIndex;
   private int[] data;
 
-  public ArraySequence(int[] other){ }
+  public ArraySequence(int[] other){
+    for(int i = 0; i < other.length; i++){
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+  public int length(){
+    return data.length;
+  }
+  public boolean hasNext(){
+    return currentIndex =< data.length;
+  }
+  public int next(){
+    if(currentIndex > data.length){
+      throw new NoSuchElementException("You have reached the end, " + end
+      + " . There is no element afterwards");
+    }
+    currentIndex = currentIndex + 1;
+    return data[currentIndex - 1];
+  }
 }
