dcd1ca3ede75a30b60e6a393fbbc33d6833ce9a3
VincentH-APCS <vhuang10@stuy.edu>
2020-12-08 20:21:49 -0500

    Tested the new ArraySequence constructor and the methods carried over from the interface

--- demo.java
+++ demo.java
@@ -1,18 +1,18 @@
 public class demo{
 
 public static void main(String[] args){
-  IntegerSequence r = new Range(10, 15);
-  while(r.hasNext()){
-    System.out.print(r.next());
-    if(r.hasNext() ){
-      System.out.print( " , ");
-    }
+  IntegerSequence r = new Range(10,20);
+  IntegerSequence as = new ArraySequence(r);
+
+  System.out.println("ArraySequence(r)");
+  while(as.hasNext()){
+    System.out.print(as.next()+", ");
   }
   System.out.println();
-  r.reset();
-  System.out.println(r.next());
-  System.out.println(r.next());
-  System.out.println(r.length());
-}
-
+  as.reset();
+  System.out.println(as.next());
+  System.out.println(as.next());
+  System.out.println(as.hasNext());
+  System.out.println(as.length());
+  }
 }
